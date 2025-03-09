8b48067f344c8ae13ef7c3e0eb390055ecd30b5e
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2025-03-09 17:31:52 +0900

    Add docs/setup.sh

---
+++ docs/setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+if [ "$(whoami)" != "ubuntu" ]; then
+  echo "This script must be run as ubuntu user" >&2
+  exit 1
+fi
+
+# ソースコードのダウンロード
+cd /home/ubuntu/environment
+if [ ! -d "training-llm-application-development-starter" ]; then
+  git clone https://github.com/GenerativeAgents/training-llm-application-development-starter.git
+fi
+cd training-llm-application-development-starter
+
+# uvのインストール
+curl -LsSf https://astral.sh/uv/0.4.14/install.sh | sh
+export PATH="$HOME/.cargo/bin:$PATH"
+uv --version
+
+# PythonとPythonパッケージのインストール
+uv sync
+uv run python --version
+
+# langchainリポジトリのclone
+if [ ! -d "./tmp/langchain" ]; then
+  git clone --depth 1 https://github.com/langchain-ai/langchain.git ./tmp/langchain
+fi
+
+# Visual Studio Codeの拡張機能のインストール
+recommendations=(
+  "charliermarsh.ruff"
+  "ms-python.mypy-type-checker"
+  "ms-toolsai.jupyter"
+)
+for recommendation in "${recommendations[@]}"; do
+  code-server --install-extension "${recommendation}" --force
+done
